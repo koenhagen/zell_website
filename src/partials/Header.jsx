2858e4042b347020316c804db0f181eb45b775b2
koenhagen <koenhagen@gmail.com>
2023-03-07 15:47:49 +0100

    Open Zell

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -45,7 +45,7 @@ function Header() {
 
               <li>
                 <Link to="/signup" className="btn-sm text-white bg-orange-600 hover:bg-orange-700 ml-3">
-                  <span>Open Zell in your browser</span>
+                  <span>Open Zell</span>
                   <svg className="w-3 h-3 fill-current text-white flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                   </svg>                  
